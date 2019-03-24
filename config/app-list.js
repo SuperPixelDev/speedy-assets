8c5d46df19ef3547c36603963eb636f5cd178743
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-03-24 20:03:47 +0900

    Update app-list.js

--- config/app-list.js
+++ config/app-list.js
@@ -1,2 +1,2 @@
-SPEEDY_App_Add('speedy', 'SPEEDY Introduction', 'SPD the Super Pixel Dev.');
+SPEEDY_App_Add('speedy', 'SPEEDY Developer Section', 'SPD the Super Pixel Dev.');
 SPEEDY_App_Add('scratchmitedu', 'Scratch for SPEEDY', 'MIT');
