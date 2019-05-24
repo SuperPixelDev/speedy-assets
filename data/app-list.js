7939d0494b282300005952e5ecf69129df197b2d
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-05-25 00:10:38 +0900

    Create app-list.js

---
+++ data/app-list.js
@@ -0,0 +1,3 @@
+SPEEDY_Server_Add('speedy', 'SPEEDY Introduction', 'speedy.spd.thepixelplaces.com');
+SPEEDY_Server_Add('qcks-spd', 'QCKS (Qingdao Chungwoon Korean School/칭다오청운한국학교)', 'qchwn.org');
+SPEEDY_Server_Add('scratchmitedu', 'Scratch for SPEEDY', 'mit.edu');
