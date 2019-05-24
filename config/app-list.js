52121bfc937e368b2f0666daf03ad3d1586fcfc6
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-05-25 00:11:25 +0900

    Delete app-list.js

--- config/app-list.js
+++
@@ -1,3 +0,0 @@
-SPEEDY_App_Add('speedy', 'SPEEDY Developer Section', 'SPD the Super Pixel Dev.');
-SPEEDY_App_Add('scratchmitedu', 'Scratch for SPEEDY', 'MIT');
-SPEEDY_App_Add('2048', '2048 the Game', 'Gabriele Cirulli');
