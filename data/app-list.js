5d5dc3b9fdcc211a3f657f3b7c76084dd392e92f
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-05-28 12:32:52 +0800

    Update app-list.js

--- data/app-list.js
+++ data/app-list.js
@@ -1,3 +1,6 @@
 SPEEDY_Server_Add('speedy', 'SPEEDY Introduction', 'speedy.spd.thepixelplaces.com');
 SPEEDY_Server_Add('qcks-spd', 'QCKS (Qingdao Chungwoon Korean School/칭다오청운한국학교)', 'qchwn.org');
 SPEEDY_Server_Add('scratchmitedu', 'Scratch for SPEEDY', 'mit.edu');
+SPEEDY_Server_Add('hextris', 'Hextris the Game', 'game.spd.thepixelplaces.com');
+SPEEDY_Server_Add('ponyvoltexpixelcom', 'Pony.VoltexPixel.com', 'pony.voltexpixel.com');
+
